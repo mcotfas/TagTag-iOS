13211bca52e109aa8bb8a034c5024eba38e56cd9
Marius Cotfas <marius@cotfas.com>
2024-03-07 23:07:57 +0100

    NO-Ticket Setup Client and User app

---
+++ TagTag-Client/TagTag-Client/TagTag_ClientApp.swift
@@ -0,0 +1,16 @@
+//
+// Created by Marius Cotfas
+// 
+//
+    
+
+import SwiftUI
+
+@main
+struct TagTag_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TagTag-User/TagTag-User/TagTag_UserApp.swift
@@ -0,0 +1,16 @@
+//
+// Created by Marius Cotfas
+// 
+//
+    
+
+import SwiftUI
+
+@main
+struct TagTag_UserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
